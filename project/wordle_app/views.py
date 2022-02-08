19e84d88085dc96d476369f74d56aab54d8a1edd
Harrison Lau <harrison.lau@outlook.com>
2022-02-08 23:05:48 +0800

    feat: render keyboard_dict

--- project/wordle_app/views.py
+++ project/wordle_app/views.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic.edit import UpdateView
 from django.contrib import messages
 from .models import Game
+from .utils import transform_data
 
 # Create your views here.
 
@@ -35,13 +36,25 @@ class GameUpdateView(UpdateView):
         request.POST = request.POST.copy()
         prefix = self.object.attempts + "," if self.object.attempts else ""
         request.POST["attempts"] = prefix + request.POST["attempts"]
+        # TODO: Increment tries
 
         return super(GameUpdateView, self).post(request, *args, **kwargs)
+
+    # TODO: Return few things:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        word = self.object.word.word
+        attempts = self.object.attempts
+        [attempts_list, keyboard_list] = transform_data(word, attempts)
+        context["attempts_list"] = attempts_list
+        context["keyboard_list"] = keyboard_list
+        return context
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         return super().get(request, *args, **kwargs)
 
+    # TODO: Invalid based on word length (to scale up to more than 5 characters words)
     def form_invalid(self, form):
         response = super().form_invalid(form)
         if response.status_code is not 200 or form.errors:
