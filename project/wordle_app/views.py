97f11fb161205f8ba61a096a9d1e5e7d4fc13f17
Harrison Lau <harrison.lau@outlook.com>
2022-02-09 22:03:47 +0800

    feat: redirect to old or new game

--- project/wordle_app/views.py
+++ project/wordle_app/views.py
@@ -1,6 +1,6 @@
 import pdb
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic.edit import UpdateView
 from django.contrib import messages
@@ -10,26 +10,44 @@ from .utils import transform_data
 # Create your views here.
 
 
+class ExpiredGameException(Exception):
+    pass
+
+
+# If user is logged in, redirect to valid game (old or new)
+# If user is not logged in, return index
 def homepage(request):
-    # See if user is playing a valid game first
     try:
         game = Game.objects.filter(player=request.user.id).latest("created_at")
         if not game.is_valid():
-            raise "Game expired, start new game"
-    except ObjectDoesNotExist:
+            # TODO: not getting to except clause
+            # raise ExpiredGameException("Game has expired")
+            pass
+    except ObjectDoesNotExist or ExpiredGameException:
         # Otherwise create game for user
         game = Game.start_game(request)
-    return render(
-        request,
-        "wordle_app/index.html",
-        context={"game": game, "types": ["correct", "present", "absent"]},
-    )
+    return redirect("wordle_app:game", game.id)
+
+    # # For not logged in users:
+    # return render(
+    #     request,
+    #     "wordle_app/index.html",
+    #     context={"game": game},
+    # )
 
 
 class GameUpdateView(UpdateView):
     model = Game
     fields = ["attempts"]
     template_name = "wordle_app/index.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not self.object.is_valid():
+            self.object = Game.start_game(request)
+            pdb.set_trace()
+            kwargs["pk"] = self.object.id
+        return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
@@ -40,7 +58,6 @@ class GameUpdateView(UpdateView):
 
         return super(GameUpdateView, self).post(request, *args, **kwargs)
 
-    # TODO: Return few things:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         word = self.object.word.word
@@ -48,11 +65,10 @@ class GameUpdateView(UpdateView):
         [attempts_list, keyboard_list] = transform_data(word, attempts)
         context["attempts_list"] = attempts_list
         context["keyboard_list"] = keyboard_list
+        context["remaining_attempts"] = (
+            range(6 - len(attempts_list)) if len(attempts_list) >= 1 else range(6)
+        )
         return context
-
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        return super().get(request, *args, **kwargs)
 
     # TODO: Invalid based on word length (to scale up to more than 5 characters words)
     def form_invalid(self, form):
