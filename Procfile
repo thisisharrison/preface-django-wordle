548de32b3aca3a409a851c5bea7a989b63a751a0
Harrison Lau <harrison.lau@outlook.com>
2022-02-17 19:15:05 +0800

    refactor: heroku configs part 2

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: python project/manage.py runserver 0.0.0.0:$PORT
+release: python manage.py migrate
--- project/project/settings.py
+++ project/project/settings.py
@@ -15,7 +15,6 @@ import os
 import io
 
 import environ
-from google.cloud import secretmanager
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,22 +30,8 @@ env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
 
 if os.path.isfile(env_file):
-    # Use a local secret file, if provided
-
     env.read_env(env_file)
-# ...
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 SECRET_KEY = env("SECRET_KEY")
 
@@ -113,9 +98,6 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 # Database
 
-# [START dbconfig]
-# [START gaeflex_py_django_database_config]
-# Use django-environ to parse the connection string
 if DEBUG:
     DATABASES = {
         "default": {
@@ -123,16 +105,6 @@ if DEBUG:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-else:
-    DATABASES = {"default": env.db()}
-
-    # If the flag as been set, configure to use proxy
-    if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
-        DATABASES["default"]["HOST"] = "127.0.0.1"
-        DATABASES["default"]["PORT"] = 5432
-
-# [END gaeflex_py_django_database_config]
-# [END dbconfig]
 
 
 # Password validation
@@ -169,20 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# [START staticurl]
-# [START gaeflex_py_django_static_config]
-# Define static storage via django-storages[google]
 STATIC_URL = "static/"
 STATICFILES_DIRS = [STATIC_DIR]
-
-if not DEBUG:
-    GS_BUCKET_NAME = env("GS_BUCKET_NAME")
-    DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-    STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-    GS_DEFAULT_ACL = "publicRead"
-
-# [END gaeflex_py_django_static_config]
-# [END staticurl]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
